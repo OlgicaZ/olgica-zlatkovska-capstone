1390bdc7d66026cb0ad256d3950d6ea1f90d3510
Олгица Златковска <olgica@Olgicas-MBP.localdomain>
2023-11-20 15:44:38 -0500

    create menu_items migrations

--- server/migrations/20231120193232_create_menu_items_table.js
+++ server/migrations/20231120193232_create_menu_items_table.js
@@ -1,15 +1,20 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('menu_items', (table) => {
+        table.increments('id').primary();
+        table
+            .integer('restaurant_id')
+            .unsigned()
+            .references('restaurants.id')
+            .onUpdate('CAS  CADE')
+            .onDelete('CASCADE');
+        table.string('menu_item_name').notNullable();
+        table.string('description').notNullable();
+        table.decimal('price', 10, 2).notNullable();
+        table.string('category').notNullable();
+    })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('menu_items');
 };
